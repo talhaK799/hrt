f92d4ccb164583c2858bfd3969cbb8fef4040243
Eagle799 <talhakhurshed799@gmail.com>
2023-10-14 18:50:09 +0500

    filter backend in progress

---
+++ lib/core/models/filter.dart
@@ -0,0 +1,15 @@
+class Filtering {
+  int? maxAge;
+  int? minAge;
+  // double? maxHeight;
+  // double? minHeight;
+  // List<String>? countries;
+
+  Filtering({
+    // this.countries,
+    this.maxAge = 60,
+    // this.maxHeight = 200,
+    this.minAge = 1,
+    // this.minHeight = 1,
+  });
+}
